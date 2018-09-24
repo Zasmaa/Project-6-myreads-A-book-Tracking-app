72be9422cf6ddcd9300cceff1e443cb5ee1ef18d
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-09-24 16:43:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
-import React from 'react'
+import React from 'react';
 import BookShelf from './MainPage/BookShelf';
-import searchPage from './SearchPage/searchPage';
+import SearchPage from './SearchPage/SearchPage';
+import bookkList from './MainPage/bookList';
+import Shelf from './MainPage/Shelf'
 import * as BooksAPI from './BooksAPI'
+import {Switch, Route} from 'react-router-dom'
 import './App.css'
 
-class BooksApp extends React.Component {
-  state = {
- books:[]
+
+class BooksApp extends React.Component { 
+constructor(props){
+  super(props)
+   this.state = {
+   books :[]
   }
 
+}
+
 componentDidMount(){
-  BooksAPI.getAll().then((books) =>{
-   this.setState({books:books}) 
+  BooksAPI.getAll().then((books) =>{ 
+   this.setState({books}) 
   })
 }
 
   render() {
+    console.log(this.state.books)
     return (
-      <div className="app">
-      
-    <Shelf
-      books = {this.state.Books}
+   <div className="app">
+   
+   
+
+<Route exact path='/' render={()=>
+  <Shelf
+      books = {this.state.books}
       />
-          
+
+}
+/>
+   
+
+}/>
+
       </div>
-    )
+
+
+
+
+      )
+
+      
   }
 }
 

f81bc2c68043946339b7631328764f794ec44ae4
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-10-02 23:56:11 -0500

    Update Shelf.js

--- src/MainPage/Shelf.js
+++ src/MainPage/Shelf.js
@@ -2,58 +2,32 @@ import React, {component} from 'react';
 import Book from "./bookList"
 import BookShelf from './BookShelf';
 
-class Shelf extends React.Component{
-  render(){
-    return(
 
+class Shelf extends React.Component{
+  componentDidMount(){
+    console.log(this)
+}
+  render(){
+    
+    return(
       
       <div>
-
         <div className="bookshelf">
-          <h2 className="bookshelf-title">Currently Reading</h2>
+          <h2 className="bookshelf-title">{this.props.title}</h2>
           <div className="bookshelf-books">
             <ol className="books-grid">
 
-
-
-      {this.props.books.map(book => (
-<li key={book.id}>
-<Book 
-book={book}
-
-/>
-</li>
+{this.props.books.map(book => (
+<li key={book.id}><Book /></li>
 ))}
 
-              
-
-
-            </ol>
-          </div>
-        </div>
-
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Want to Read</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-
-
     
             </ol>
           </div>
         </div>
+       
+
         
-        <div className="bookshelf">
-          <h2 className="bookshelf-title">Read</h2>
-          <div className="bookshelf-books">
-            <ol className="books-grid">
-             
-
-    
-
-            </ol>
-          </div>
-        </div>
       </div>
     
       )

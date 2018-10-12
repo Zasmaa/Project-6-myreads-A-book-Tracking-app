6c91e7c4856618b22970a1b23bc46fb96250ef8a
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-10-11 20:40:09 -0500

    Update bookList.js

--- src/MainPage/bookList.js
+++ src/MainPage/bookList.js
@@ -1,13 +1,15 @@
 import React, {component} from 'react';
+import Shelf from './Shelf'
+import BookShelf from './BookShelf';
 class bookList extends React.Component{
-	componentDidMount() {console.log(this)}
-	render(){
-		return(
-						<div className="book">
+  componentDidMount() {console.log(this.props)}
+  render(){
+    return(
+            <div className="book">
                   <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
+                    <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail}"`}}></div>
                     <div className="book-shelf-changer">
-                      <select>
+                      <select value={this.props.book.shelf || "none"} onChange={(event) => this.props.changeShelf(this.props.book, event.target.value)}> 
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>
@@ -16,11 +18,11 @@ class bookList extends React.Component{
                       </select>
                     </div>
                   </div>
-                  <div className="book-title"></div>
-                  <div className="book-authors"></div>
+                  <div className="book-title">{this.props.book.title}</div>
+                  <div className="book-authors">{this.props.book.authors}</div>
                 </div>
                 
-			)
-	}
+      )
+  }
 }
 export default bookList

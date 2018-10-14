bbafcc3984d84d1b1983f59ef9e11491ef70fb7b
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-10-14 13:42:03 -0500

    Update bookList.js

--- src/MainPage/bookList.js
+++ src/MainPage/bookList.js
@@ -4,12 +4,13 @@ import BookShelf from './BookShelf';
 class bookList extends React.Component{
   componentDidMount() {console.log(this.props)}
   render(){
+    const {book, changeShelf} = this.props
     return(
             <div className="book">
                   <div className="book-top">
                     <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${this.props.book.imageLinks && this.props.book.imageLinks.thumbnail}"`}}></div>
                     <div className="book-shelf-changer">
-                      <select value={this.props.book.shelf || "none"} onChange={(event) => this.props.changeShelf(this.props.book, event.target.value)}> 
+                      <select value={this.props.book.shelf || "none"} onChange={(event) => changeShelf(book, event.target.value)}>
                         <option value="move" disabled>Move to...</option>
                         <option value="currentlyReading">Currently Reading</option>
                         <option value="wantToRead">Want to Read</option>

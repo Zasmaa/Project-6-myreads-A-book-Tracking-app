a3466b748026ea7665655df034a82d953dbab98c
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-10-28 17:11:31 -0500

    Update SearchPage.js

--- src/SearchPage/SearchPage.js
+++ src/SearchPage/SearchPage.js
@@ -1,110 +1,104 @@
 
-import React, {Compoment} from 'react';
-import {Link} from 'react-router-dom';
-import Book from '../MainPage/bookList'
-import Shelf from '../MainPage/Shelf'
-import BookShelf from '../MainPage/BookShelf'
-import * as BooksAPI from '../BooksAPI'
+
+import React, { Compoment } from "react";
+import { Link } from "react-router-dom";
+import Book from "../MainPage/bookList";
+import Shelf from "../MainPage/Shelf";
+import BookShelf from "../MainPage/BookShelf";
+import * as BooksAPI from "../BooksAPI";
 
 class SearchPage extends React.Component {
- constructor(props){
-  super(props)
-   this.state = {
-   query : '',
-   searchResults : [],
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: "",
+      searchResults: []
+    };
   }
- }
-
-
-   updateQuery = (query) => {
-    this.setState(() => ({ query: query }), this.results(query));
-  }
-
-results (query) {
-  if (query === ''){
-    this.setState ({
-      searchResults : []
-    });
-   
-    return;
-    
-  } 
-
-    BooksAPI.search(query).then(results =>{
-     console.log(results)
-    results instanceof Array
-        ? this.setState({ searchResults: results })
-        : this.setState({ searchResults: [] });
-
-      
-    });
 
 
   
-};
-//credit :  learned these from  both  ryan waite walk through and Maeva walk through : 
+  updateQuery = query => {
+    if (query === "") {
+      this.setState({
+        searchResults: []
+      });
+      return;
+    } 
+  
+    BooksAPI.search(query).then(results => {
+      results instanceof Array
+        ? this.setState({ searchResults: results })
+        : this.setState({ searchResults: [] });
+    });
+    
+  };
+ 
+  handleChange = e => {
+    this.setState({ query: e });
+    this.updateQuery(e);
+  };
+
+  //credit :  learned these from  both  ryan waite walk through and Maeva walk through : 
 //https://www.youtube.com/watch?v=acJHkd6K5kI&=&feature=youtu.be
 //https://www.youtube.com/watch?v=i6L2jLHV9j8
 //also drunkenkismet and sara-FEND help me by answering my quesiton on sluck DM. 
 
-
-
-
-
-	render(){
-
-		return(
-			<div>
-
-			<div className="search-books">
-            <div className="search-books-bar">
-            <Link className="close-search" to='/'> Close </Link>
-             <div className="search-books-input-wrapper">
-        
-                <input type="text" 
+  render() {
+    return (
+      <div>
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link className="close-search" to="/">
+              {" "}
+              Close{" "}
+            </Link>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
                 placeholder="Search by title or author"
-                value= {this.state.query}
-                 onChange = {(event) => this.updateQuery(event.target.value)}
-
-                />
-
-
-              </div>
+                value={this.state.query}
+                onChange={event => this.handleChange(event.target.value)}
+              />
             </div>
+          </div>
 
-            <div className="search-books-results">
-              <ol className="books-grid"> 
+          <div className="search-books-results">
+            <ol className="books-grid">
               {
-                this.state.searchResults.map(results =>(
-                  <li key={results.id}>
+                this.state.searchResults.map(results =>{
+                  let Shelf = "none"
+
+                  this.props.books.map(book => (
+                    book.id === searchResults.id ?
+                    Shelf = book.Shelf :
+                    ''
+                    ));
+
+
+                  return (
+                     <li key={results.id}>
                   <Book book={results}
                   changeShelf={this.props.changeShelf}
+                  currentShelf = {Shelf}
                   />
                   </li>
 
-                  ))
+                    )
+                })
               }
 
 
-              </ol>
 
 
+              
 
-            </div>
-
+            </ol>
           </div>
-
-
-
-			</div>
-
-			)
- 
-
-
-	}
-
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SearchPage

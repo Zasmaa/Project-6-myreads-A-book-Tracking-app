3bffd7883d1261494152a6723ad6298d47386110
Zasmaa <39232461+Zasmaa@users.noreply.github.com>
2018-10-30 17:11:44 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import BookShelf from './MainPage/BookShelf';
-import SearchPage from './SearchPage/SearchPage';
-import bookkList from './MainPage/bookList';
-import Shelf from './MainPage/Shelf'
-import * as BooksAPI from './BooksAPI'
-import {Switch, Route} from 'react-router-dom'
-import {Link} from 'react-router-dom'
-import './App.css'
+import React, { Compoment } from "react";
+import { Link } from "react-router-dom";
+import Book from "../MainPage/bookList";
+import Shelf from "../MainPage/Shelf";
+import BookShelf from "../MainPage/BookShelf";
+import * as BooksAPI from "../BooksAPI";
 
+class SearchPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: [],
+      query: "",
+      searchResults: []
 
-class BooksApp extends React.Component { 
-
-
-
-constructor(props){
-  super(props)
-   this.state = {
-   books :[]
+    };
   }
-
-}
 
 componentDidMount(){
   this.getBooksDetails ()
@@ -30,46 +25,99 @@ getBooksDetails = () => {
    this.setState({books}) 
   })
 }
-
- changeShelf = (book, shelf) => {
+changeSerach = (book, shelf, results) => {
      BooksAPI.update(book, shelf).then( response => {
         book.shelf =shelf;
+        book.id === results.id 
         this.setState(prevState => {
           return {books: prevState.books.concat(book)}
         })
       })
    }
  
+  
+  updateQuery = query => {
+    if (query === "") {
+      this.setState({
+        searchResults: []
+      });
+      return;
+    } 
+  
+    BooksAPI.search(query).then(results => {
+      results instanceof Array
+        ? this.setState({ searchResults: results })
+        : this.setState({ searchResults: [] });
+    });
+    
+  };
+ 
+  handleChange = e => {
+    this.setState({ query: e });
+    this.updateQuery(e);
+  };
+  
+
+  //credit :  learned these from  both  ryan waite walk through and Maeva walk through : 
+//https://www.youtube.com/watch?v=acJHkd6K5kI&=&feature=youtu.be
+//https://www.youtube.com/watch?v=i6L2jLHV9j8
+//also drunkenkismet and sara-FEND help me by answering my quesiton on sluck DM. 
 
   render() {
-    console.log(this.state.books)
     return (
-   <div className="app">
+      <div>
+        <div className="search-books">
+          <div className="search-books-bar">
+            <Link className="close-search" to="/">
+              {" "}
+              Close{" "}
+            </Link>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={this.state.query}
+                onChange={event => this.handleChange(event.target.value)}
+              />
+            </div>
+          </div>
 
-    <Route exact path='/' render={()=> (
-   <BookShelf
-  books={this.state.books}
-  changeShelf={this.changeShelf}
-  />
+          <div className="search-books-results">
+            <ol className="books-grid">
+              {
+                this.state.searchResults.map(results =>{
+                  let Shelf = "none"
+
+                  this.props.books.map(book => (
+                    book.id === results.id ?
+                    Shelf = book.Shelf :
+                    ''
+                    ));
 
 
-  )}/>
+                  return (
+                     <li key={results.id}>
+                  <Book book={results}
+                  changeShelf={this.props.changeShelf}
+                  currentShelf = {Shelf}
+                  />
+                  </li>
 
-<Route path= '/search' render={()=>(
-
-    <SearchPage
-     books={this.state.books}
-
-        changeShelf={this.changeShelf} 
-        />
+                    )
+                })
+              }
 
 
-  )}/>
 
-   </div>
-      )    
+
+              
+
+            </ol>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
-
-export default BooksApp
+export default SearchPage;
